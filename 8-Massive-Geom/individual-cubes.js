ddb0aa6e2c7713a94f9d0f55223eda79ebb6e440
bushj <bushj@moravian.edu>
2025-10-17 13:44:50 -0400

    Adding large scene demos

---
+++ 8-Massive-Geom/individual-cubes.js
@@ -0,0 +1,195 @@
+import * as THREE from 'three';
+import { GUI } from 'lil-gui';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { stats } from '../shared/stats.js';
+
+let renderer;
+let camera;
+let scene;
+
+function sceneSetup() {
+    // Add to scene
+    scene = new THREE.Scene();
+
+    // Add lights
+    const light1 = new THREE.DirectionalLight(0xffffff, 1);
+    light1.position.set(1, 1, 1);
+    light1.castShadow = true;
+    light1.shadow.camera.top = 30;
+    light1.shadow.camera.bottom = -30;
+    light1.shadow.camera.left = -30;
+    light1.shadow.camera.right = 30;
+    light1.shadow.camera.near = -30;
+    light1.shadow.camera.far = 30;
+    scene.add(light1);
+
+    const light2 = new THREE.DirectionalLight(0xffffff, 0.5);
+    light2.position.set(-1, 1, -1);
+    light2.castShadow = true;
+    light1.shadow.camera.top = 30;
+    light1.shadow.camera.bottom = -30;
+    light1.shadow.camera.left = -30;
+    light1.shadow.camera.right = 30;
+    light1.shadow.camera.near = -30;
+    light1.shadow.camera.far = 30;
+    scene.add(light2);
+
+    const ambientLight = new THREE.AmbientLight(0x606060);
+    scene.add(ambientLight);
+
+    // Set up camera
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(30, 25, 30);
+    camera.lookAt(0, 0, 0);
+
+    // Set up renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    renderer.shadowMap.enabled = false;
+    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    // Set up orbit controls
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0, 0);
+    controls.update();
+
+    // Create the base objects
+    const mat = new THREE.MeshNormalMaterial({
+        blending: THREE.NormalBlending,
+        opacity: 0.1,
+        transparent: true,
+    });
+    const group = new THREE.Group();
+    group.castShadow = true;
+    group.receiveShadow = true;
+    scene.add(group);
+    const materials = {
+        'no override': null,
+        'basic': new THREE.MeshBasicMaterial({
+            color: 0x00ff00,
+            blending: THREE.NormalBlending,
+            opacity: 0.1,
+            transparent: true,
+        }),
+        'normal': mat,
+        'phong': new THREE.MeshPhongMaterial({
+            color: 0xff0000,
+            blending: THREE.NormalBlending,
+            opacity: 0.1,
+            transparent: true,
+        }),
+        'standard': new THREE.MeshStandardMaterial({
+            color: 0x0000ff,
+            blending: THREE.NormalBlending,
+            opacity: 0.1,
+            transparent: true,
+        }),
+    }
+
+    // Parameters for new cubes
+    const newCubeParams = {
+        scaleX: 1,
+        scaleY: 1,
+        scaleZ: 1,
+        rotateX: 0,
+        rotateY: 0,
+        rotateZ: 0,
+        material: 'normal',
+    };
+
+    // Create the GUI
+    const gui = new GUI();
+    const actions = {
+        add1000: () => { createCubes(group, materials[newCubeParams.material], 1000, newCubeParams); actions.count += 1000; },
+        add100: () => { createCubes(group, materials[newCubeParams.material], 100, newCubeParams); actions.count += 100; },
+        add10: () => { createCubes(group, materials[newCubeParams.material], 10, newCubeParams); actions.count += 10; },
+        add1: () => { group.add(createCube(materials[newCubeParams.material]), newCubeParams); actions.count += 1; },
+        clear: () => {
+            while (group.children.length > 0) {
+                group.children.pop().geometry.dispose();
+            }
+            actions.count = 0;
+        },
+        count: 0,
+        material: 'no override',
+        useShadows: false,
+    };
+    gui.add(actions, 'add1000').name('Add 1000 Cubes');
+    gui.add(actions, 'add100').name('Add 100 Cubes');
+    gui.add(actions, 'add10').name('Add 10 Cubes');
+    gui.add(actions, 'add1').name('Add 1 Cube');
+    gui.add(actions, 'clear').name('Clear All Cubes');
+    gui.add(actions, 'count').name('Cube Count').listen().disable();
+    gui.add(actions, 'material', Object.keys(materials)).name('Material').onChange((value) => {
+        scene.overrideMaterial = materials[value];
+    });
+    gui.add(actions, 'useShadows').name('Use Shadows').onChange((value) => {
+        renderer.shadowMap.enabled = value;
+        renderer.shadowMap.needsUpdate = true;
+    });
+
+    // New Cube Control GUI
+    const newCubeFolder = gui.addFolder('New Cube Controls');
+    newCubeFolder.add(newCubeParams, 'scaleX', 0.1, 5);
+    newCubeFolder.add(newCubeParams, 'scaleY', 0.1, 5);
+    newCubeFolder.add(newCubeParams, 'scaleZ', 0.1, 5);
+    newCubeFolder.add(newCubeParams, 'rotateX', 0, Math.PI * 2);
+    newCubeFolder.add(newCubeParams, 'rotateY', 0, Math.PI * 2);
+    newCubeFolder.add(newCubeParams, 'rotateZ', 0, Math.PI * 2);
+    newCubeFolder.add(newCubeParams, 'material', Object.keys(materials).filter((x) => x !== "no override"));
+
+    // Group Control GUI
+    const groupParams = {
+        scaleX: 1,
+        scaleY: 1,
+        scaleZ: 1,
+        rotateX: 0,
+        rotateY: 0,
+        rotateZ: 0,
+    };
+    const groupControlFolder = gui.addFolder('Group Controls');
+    groupControlFolder.add(groupParams, 'scaleX', 0.1, 5).onChange((value) => { group.scale.x = value; });
+    groupControlFolder.add(groupParams, 'scaleY', 0.1, 5).onChange((value) => { group.scale.y = value; });
+    groupControlFolder.add(groupParams, 'scaleZ', 0.1, 5).onChange((value) => { group.scale.z = value; });
+    groupControlFolder.add(groupParams, 'rotateX', 0, Math.PI * 2).onChange((value) => { group.rotation.x = value; });
+    groupControlFolder.add(groupParams, 'rotateY', 0, Math.PI * 2).onChange((value) => { group.rotation.y = value; });
+    groupControlFolder.add(groupParams, 'rotateZ', 0, Math.PI * 2).onChange((value) => { group.rotation.z = value; });
+
+    // Render loop
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+        stats.update();
+        controls.update();
+    }
+    animate();
+}
+document.addEventListener('DOMContentLoaded', sceneSetup);
+window.addEventListener('resize', () => {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+function createCubes(group, mat, n, params) {
+    for (let i = 0; i < n; i++) {
+        group.add(createCube(mat, params));
+    }
+}
+
+function createCube(mat, params) {
+    const range = 20;
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const mesh = new THREE.Mesh(geometry, mat);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.position.set(
+        (Math.random() - 0.5) * range,
+        (Math.random() - 0.5) * range,
+        (Math.random() - 0.5) * range
+    );
+    mesh.scale.set(params.scaleX, params.scaleY, params.scaleZ);
+    mesh.rotation.set(params.rotateX, params.rotateY, params.rotateZ);
+    return mesh;
+}
